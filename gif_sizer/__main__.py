c4f41839a9a11ce35acf7b39d1f5020634492af2
msmukowski <msmukowski@gmail.com>
2023-01-26 00:52:26 +0100

    feat(dev-tools) installed styling tools and linters

--- gif_sizer/__main__.py
+++ gif_sizer/__main__.py
@@ -1,16 +1,23 @@
-import click
 from typing import Any
+
+import click
+
 from .parser import parse_command
 
 
-@click.command(help='Resize an animated GIF')
+@click.command(help="Resize an animated GIF")
 @click.argument("command")
-@click.option('--percentage', help='Specifies the percentage to which it should be reduced relative to the original GIF', is_flag=False, required=True, type=int)
-@click.option("--verbose", "-v", help='Enable verbose output', is_flag=True, default=False)
-def main(
-    command: str,
-    **kwargs: dict[str, Any]
-) -> None:
+@click.option(
+    "--percentage",
+    help="Specifies the percentage to which it should be reduced relative to the original GIF",
+    is_flag=False,
+    required=True,
+    type=int,
+)
+@click.option(
+    "--verbose", "-v", help="Enable verbose output", is_flag=True, default=False
+)
+def main(command: str, **kwargs: dict[str, Any]) -> None:
     parsed_command = parse_command(command, **kwargs)
 
 
--- gif_sizer/parser.py
+++ gif_sizer/parser.py
@@ -1,7 +1,7 @@
-import click
 from typing import Any
 
 ACCEPTED_COMMANDS = ("resize", "rescale")
+
 
 def parse_command(command: str, **kwargs: dict[str, Any]) -> dict:
     _validate_command(command)
@@ -9,6 +9,7 @@ def parse_command(command: str, **kwargs: dict[str, Any]) -> dict:
 
 
 def _validate_command(command: str) -> None:
-    if not command in ACCEPTED_COMMANDS:
-        raise KeyError(f"Given command: '{command}' is not allowed! Allowed commands: {ACCEPTED_COMMANDS}.")
-    
+    if command not in ACCEPTED_COMMANDS:
+        raise KeyError(
+            f"Given command: '{command}' is not allowed! Allowed commands: {ACCEPTED_COMMANDS}."
+        )
