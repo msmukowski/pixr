4db63f1bc3d0a7c54574e980fa5ce399f9b4553f
msmukowski <msmukowski@gmail.com>
2023-02-02 23:50:10 +0100

    feat(tests): test unexpected RunnerFactory case

--- tests/test_runner_factory.py
+++ tests/test_runner_factory.py
@@ -26,3 +26,21 @@ from gif_sizer.runner_factory import RunnerFactory
 def test_runner_factory(command, expected_runner):
     runner = RunnerFactory.create_runner(command)
     assert runner == expected_runner
+
+
+@pytest.mark.parametrize(
+    "command, expected_error, expected_msg",
+    [
+        (
+            Command(
+                CmdArgument(**{"resize": "test"}),
+                CmdOptions(**{"percentage": 50, "verbose": False}),
+            ),
+            ValueError,
+            "Unhandled creation command argument: 'test'!",
+        ),
+    ],
+)
+def test_runner_factory_value_error(command, expected_error, expected_msg):
+    with pytest.raises(expected_error, match=expected_msg):
+        RunnerFactory.create_runner(command)
